92d0b59ae4970361134e807bab33c063902dcbdb
user <mauricionrk@gmail.com>
2023-04-26 16:13:56 -0300

    Add main.js and validations on Vue and endpoint

---
+++ public/main.js
@@ -0,0 +1,54 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      idx: 0,
+      selectedAnswer: "",
+      correctAnswers: 0,
+      wrongAnswers: 0,
+      questionsLength: 0,
+      questions: [],
+    };
+  },
+  methods: {
+    answered(e) {
+      this.selectedAnswer = e.target.value;
+      if (this.selectedAnswer == this.questions[this.idx].correctAnswer) {
+        this.correctAnswers++;
+      } else {
+        this.wrongAnswers++;
+      }
+    },
+    nextQuestion() {
+      this.idx++;
+      this.selectedAnswer = "";
+      document.querySelectorAll("input").forEach((el) => (el.checked = false));
+    },
+    showResults() {
+      this.idx++;
+    },
+    resetQuiz() {
+      this.idx = 0;
+      this.selectedAnswer = "";
+      this.correctAnswers = 0;
+      this.wrongAnswers = 0;
+    },
+  },
+  created() {
+    fetch('http://localhost:3001/')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        this.questions = data;
+        this.questionsLength = data.length;
+      })
+      .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+      });
+  },
+});
+
+app.mount("#app");
